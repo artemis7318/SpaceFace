387b1f181d26a30a393d4a8da0c5a2f4a945055b
artemis7318 <angelalou777@gmail.com>
2023-09-16 14:06:07 -0400

    add pages

---
+++ demo.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $('.gallery').cycle({
+		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+
+});
+
+$(document).ready(function() {
+
+    $('.first figure').cycle({
+		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+
+});
+
+$(document).ready(function() {
+
+    $('.second figure').cycle({
+		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+
+});
+
+$(document).ready(function() {
+
+    $('.third figure').cycle({
+		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+
+});

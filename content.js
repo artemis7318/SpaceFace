bc91fabcbd242011369e194258740c28775151f7
Melody Hu <40373356+shinyumh@users.noreply.github.com>
2023-09-16 11:06:43 -0400

    change files location

--- content.js
+++
@@ -1,6 +0,0 @@
-document.body.style.backgroundColor = 'white';
-console.log("CONTENT");
-
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-    console.log(response.farewell);
-})
